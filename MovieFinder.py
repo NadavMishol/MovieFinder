895df34a27a51c1f6746033a8b023937157115c7
NadavMishol <nadavmishol@gmail.com>
2024-02-26 13:58:42 +0200

    main jupyter code

---
+++ MovieFinder.py
@@ -0,0 +1,23 @@
+import gzip
+import shutil
+import requests
+import csv
+from io import StringIO
+
+URL = 'https://datasets.imdbws.com/title.basics.tsv.gz'
+output_file = 'title.basics.tsv'
+
+response = requests.get(URL)
+
+with open('title.basics.tsv.gz', 'wb') as f:
+    f.write(response.content)
+
+# Unzip the file
+with gzip.open('title.basics.tsv', 'rb') as f_in:
+    with open(output_file, 'wb') as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
+with open(output_file, 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='\t')
+    for _ in range(10):
+        print(next(reader))
